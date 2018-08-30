0736b0db39535018f14a2a0f6233c9cd0e3aa3d8
Samuel Akintunde <samakintunde37@gmail.com>
2018-08-30 21:06:57 +0100

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello World!</h1>
+    <p>I'm Samuel Akintunde and this is my first Gatsby site!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
